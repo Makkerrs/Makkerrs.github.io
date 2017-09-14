dce6685ea08d93df10a6a64f74e4d91e99b4baad
markkerrs <qmbabc111@sina.com>
2017-09-15 00:25:20 +0800

    -a

--- vue/js/index.js
+++ vue/js/index.js
@@ -2,7 +2,8 @@ var vm = new Vue({
 	el: '#app',
 	data: { //
 		totalMoney: 0,
-		productList: []
+		productList: [],
+		checkAllFlag:false
 	},
 	filters: { //
 		formatMoney: function(value) {
@@ -15,7 +16,7 @@ var vm = new Vue({
 	methods: {
 		cartView: function() {
 			let _this = this; //存储实例化的对象指向vm
-			this.$http.get("data/cartData.json").then(res => {
+			this.$http.get("data/cartData.json",{"id":123}).then(res => {
 					console.log(res.data.result.list)
 					this.productList = res.data.result.list;
 					this.totalMoney = res.data.result.totalMoney;
@@ -35,12 +36,24 @@ var vm = new Vue({
 
 		selectedProduct: function(item) {
 			if(typeof item.checked == 'undefined') {
+			  //    设置    目标         要添加的变量   变量中 要添加的值
 			  //Vue.set(item, "checked", true);
 				this.$set(item, "checked", true);
 			} else {
 				item.checked = !item.checked;
 			}
 		},
+		checkAll:function(flag){
+			this.checkAllFlag = flag;//每次点击取反
+			var _this = this;
+			this.productList.forEach(function(item,index){
+				if(typeof item.check == 'undefined'){
+					_this.$set(item,"checked",_this.checkAllFlag);
+				}else{
+					item.checked = _this.checkAllFlag;
+				}
+			});
+		}
 	}
 })
 
